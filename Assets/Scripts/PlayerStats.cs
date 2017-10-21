a5f844eb17842537d904201b6eede8a2406ba9d5
CodyMaughan <shilohmoore@rocketmail.com>
2017-10-21 03:55:44 -0600

    Removed Debug Lines

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -64,7 +64,6 @@ public class PlayerStats : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         speed = 5;
         knockAcceleration = 2 * (knockBackDistance - initialKnockbackVelocity * timeKnocked) / (timeKnocked * timeKnocked);
-        Debug.Log(knockAcceleration);
         bagIsRight = false;
         baseDamage = 10;
         bagLeftPosition = new Vector3(-0.3209f, 0.75f, -0.1936f);
@@ -123,8 +122,6 @@ public class PlayerStats : MonoBehaviour
 
     public void TakeDamage(float damageAmt)
     {
-        Debug.Log(damageAmt);
-        Debug.Log(isInvincible);
         SoundEffectManager.instance.PlayTakeDamage();
         if (!isInvincible)
         {
@@ -296,7 +293,6 @@ public class PlayerStats : MonoBehaviour
         if(rotateBag)
         {
             float angle = bag.transform.localEulerAngles.y;
-            Debug.Log(angle);
             if (angle > 180)
             {
                 angle = angle - 360;
