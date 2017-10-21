ebf9a07552116891b1edfb4a2402835d4b7b68e5
u1065909 <u1065909@utah.edu>
2017-10-20 20:39:39 -0600

    Started on PlayerStats and PlayerController
    Player can move and is able to upgrade it's speed defense and candy capacity,

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    PlayerStats stats;
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Move();
+	}
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.W))
+            transform.position += Vector3.forward * Time.deltaTime * stats.speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.back * Time.deltaTime * stats.speed;
+        if (Input.GetKey(KeyCode.A))
+            transform.position += Vector3.left * Time.deltaTime * stats.speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * Time.deltaTime * stats.speed;
+    }
+
+}
---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+
+    [SerializeField]
+    private uint defense;
+    [SerializeField]
+    private uint candyCapacity;
+    [SerializeField]
+    private uint maxDefense;
+    [SerializeField]
+    private uint maxSpeed;
+    [SerializeField]
+    private uint maxCandyCapacity;
+    public uint speed { get; private set; }
+    private uint candyAmt;
+
+	// Use this for initialization
+	void Start ()
+    {
+        speed = 5;
+        candyAmt = 1;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void Die()
+    {
+        //Call GameManager object that kills the player
+    }
+    public void UpgradeSpeed(uint increaseAmt)
+    {
+        if (speed + increaseAmt >= maxSpeed)
+            speed = maxSpeed;
+        else
+            speed += increaseAmt;
+    }
+    public void UpgradeDefense(uint increaseAmt)
+    {
+        if (defense + increaseAmt >= maxDefense)
+            defense = maxDefense;
+        else
+            defense += increaseAmt;   
+    }
+    public void UpgradeCandyCapacity(uint increaseAmt)
+    {
+        if (candyCapacity + increaseAmt >= maxCandyCapacity)
+            candyCapacity = maxCandyCapacity;
+        else
+            candyCapacity += increaseAmt;
+    }
+
+    public void TakeDamage(uint DamageAmt)
+    {
+        candyAmt -= DamageAmt;
+        //Spawn Candy
+    }
+    public void AddCandy()
+    {
+        
+    }
+
+}
